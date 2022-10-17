c43c85bf4d23fc20fb073de4d1747c00cf265309
samuel <samuelsontsf@gmail.com>
2022-10-17 17:10:33 -0400

    Add title, icon, players, ball, center line, and movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import library
 import pygame
+import random as rd
 
 # Initialize pygame
 pygame.init()
@@ -17,6 +18,8 @@ players_height = 90
 # Player 1 coordinates
 player_1_x = 50
 player_1_y = 240
+player_1_y_speed = 0
+player_2_y_speed = 0
 
 # Player 2 coordinates
 player_2_x = 750
@@ -26,6 +29,9 @@ player_2_y = 240
 ball_x = 400
 ball_y = 300
 ball_radius = 18
+
+ball_speed_x = 0.1
+ball_speed_y = 0.1
 
 # window size
 screen_width = 800
@@ -51,6 +57,67 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # Player key controls
+
+        # Cheks for KEYDOWN event
+        if event.type == pygame.KEYDOWN:
+
+            # Player 1
+            if event.key == pygame.K_w:
+                player_1_y_speed = -5
+
+            if event.key == pygame.K_s:
+                player_1_y_speed = 5
+
+
+            # Player 2
+            if event.key == pygame.K_UP:
+                player_2_y_speed = -5
+
+            if event.key == pygame.K_DOWN:
+                player_2_y_speed = 5
+
+        if event.type == pygame.KEYUP:
+            
+            # Player 1
+            if event.key == pygame.K_w:
+                player_1_y_speed = -0
+
+            if event.key == pygame.K_s:
+                player_1_y_speed = 0
+
+            # Player 2
+            if event.key == pygame.K_UP:
+                player_2_y_speed = -0
+
+            if event.key == pygame.K_DOWN:
+                player_2_y_speed = 0
+
+    # Player movement
+    player_1_y += player_1_y_speed
+    player_2_y += player_2_y_speed
+
+    # Ball movement
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+
+    # Ball boundaries: top or buttom
+    if ball_y > (screen_height - ball_radius) or ball_y < ball_radius:
+        ball_speed_y *= -1
+
+    # Ball boudaries (right or left) and score update
+    if ball_x > screen_width:
+
+        ball_x = screen_width/2
+        ball_y = screen_height/2
+        ball_speed_x *= rd.choice([-1, 1])
+
+    elif ball_x < 0:
+
+        ball_x = screen_width/2
+        ball_y = screen_height/2
+        ball_speed_x *= rd.choice([-1, 1])
 
 # Fill the screen with color
     screen.fill( background_color )
